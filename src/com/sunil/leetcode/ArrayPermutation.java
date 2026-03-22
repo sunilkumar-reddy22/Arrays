b991ec776ee70a5742af1640f153233e4d77a0f0
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-22 09:50:51 +0530

    array permutation problem,buliding new array and getting values from the index

---
+++ src/com/sunil/leetcode/ArrayPermutation.java
@@ -0,0 +1,17 @@
+package com.sunil.leetcode;
+import java.util.Arrays;
+public class ArrayPermutation {
+    public static void main(String[] args) {
+        int [] arr = {0,2,1,5,3,4};
+        System.out.println(Arrays.toString(change(arr)));
+    }
+    static int [] change(int [] nums){
+    int [] ans = new int[nums.length];
+    for(int i = 0; i < nums.length; i++) {
+        ans[i] = nums[nums[i]];
+    }
+    return ans;
+
+
+    }
+}
