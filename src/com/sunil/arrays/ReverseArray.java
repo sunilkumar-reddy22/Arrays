15ef22cf393e81ff741354bc81dd10057a9d69b1
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-27 23:26:57 +0530

    Reverse array

--- src/com/sunil/arrays/ReverseArray.java
+++ src/com/sunil/arrays/ReverseArray.java
@@ -1,6 +1,15 @@
 package com.sunil.arrays;
-public class ReverseArray {
+public class  ReverseArray {
     public static void main(String[] args) {
-
+     int  [] arr = {2,3,4,5};
+     int start = 0;
+     int end = arr.length-1;
+     while(start < end) {
+         int temp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = temp;
+         start++;
+         end--;
+     }
     }
 }
