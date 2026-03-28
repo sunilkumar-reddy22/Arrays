709696875d71ee9e7e6496934234eaac32193653
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-28 09:09:14 +0530

    refactor: replaced old program with new implementation

--- src/com/sunil/arrays/ReverseArray.java
+++ src/com/sunil/arrays/ReverseArray.java
@@ -1,15 +1,18 @@
 package com.sunil.arrays;
+import java.util.Arrays;
 public class  ReverseArray {
     public static void main(String[] args) {
-     int  [] arr = {2,3,4,5};
-     int start = 0;
-     int end = arr.length-1;
-     while(start < end) {
-         int temp = arr[start];
-         arr[start] = arr[end];
-         arr[end] = temp;
-         start++;
-         end--;
-     }
+        int[] arr = {1, 2, 3, 4, 5};
+        int start = 0;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString((arr)));
+
     }
 }
