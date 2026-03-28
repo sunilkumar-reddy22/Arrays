7e7d60698bd60b50778c4cade13797ffe103a19a
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-28 14:06:04 +0530

    Finding even number average with counting

---
+++ src/com/sunil/arrays/AveragEvenNums.java
@@ -0,0 +1,28 @@
+package com.sunil.arrays;
+
+public class AveragEvenNums {
+    public static void main(String[] args) {
+    int [] arr = {10,55,60,23,88,41};
+    Findeven(arr);
+
+    }
+    static void Findeven(int [] nums){
+    int sum = 0;
+    int count = 0;
+    for(int i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0) {
+            sum = sum + nums[i];
+            count++;
+        }
+    }
+    if(count == 0) {
+        System.out.println("no even number found");
+        return;
+    }
+   double average = (double) sum / count;
+        System.out.println("Count = " + count);
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + average);
+
+    }
+}
