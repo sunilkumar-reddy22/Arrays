edc28b6275ae7878a76f5dff614995b97260b584
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-03-28 09:31:55 +0530

    Marks average program in arrays using functions

---
+++ src/com/sunil/arrays/AverageMarks.java
@@ -0,0 +1,16 @@
+package com.sunil.arrays;
+public class AverageMarks {
+    public static void main(String[] args) {
+        int [] marks = {70,80,92,45,65};
+        nums(marks);
+    }
+    static void  nums(int [] sum) {
+        int result = 0;
+        for (int i = 0; i < sum.length; i++) {
+            result = result + sum[i];
+        }
+        double average = result / sum.length;
+        System.out.println("Average = " + average);
+    }
+
+}
