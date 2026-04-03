7a6dd3c5263b89ba7b10f14bf000a52bd09db14f
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-03 22:12:13 +0530

    Added 2 dimentional array

---
+++ src/com/sunil/arrays/MatrixdimensionalArray.java
@@ -0,0 +1,22 @@
+package com.sunil.arrays;
+import java.util.Scanner;
+public class MatrixdimensionalArray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int [] [] arr = new int [3] [3];
+            Matrix(arr,in);
+        }
+       static  void Matrix (int [] [] nums,Scanner in){
+        for(int row = 0; row <nums.length; row++) {
+            for (int col = 0; col <nums [row].length; col++) {
+                nums  [row][col] = in.nextInt();
+            }
+        }
+        for (int row = 0; row <nums.length; row++) {
+            for (int col = 0; col < nums[row].length; col++) {
+                System.out.print(nums[row][col] + " ");
+            }
+            System.out.println();
+        }
+     }
+}
