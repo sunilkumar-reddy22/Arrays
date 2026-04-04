8b7122840e9ce76fa83111fd941c2df27635e928
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-04 17:09:12 +0530

    reversing the nums in arraylist

--- src/com/sunil/arrays/ArrayListExample.java
+++ src/com/sunil/arrays/ArrayListExample.java
@@ -11,7 +11,13 @@ public class ArrayListExample  {
         list.add(3);
         list.add(4);
         list.add(5);
-        list.add(6);
+        list.add(6);  // list.contains(); is an fucntion the checks the specific value that exits or not if it is or not gives true/false.
+        System.out.println(list);
+        //functions
+        //list is reference varible and the . is  accessing the functions to list inside values so it does the operations.
+        list.set(0,4);
+        // To remove a value from the variale you can use function call .remoe/pass the valuse in (2); and it removes and get updated.
+        list.remove(2);
         System.out.println(list);
 
     }
---
+++ src/com/sunil/arrays/Listexample.java
@@ -0,0 +1,13 @@
+package com.sunil.arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Listexample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 10; i>0; i--){
+            System.out.print(i + "  ");
+        }
+      // System.out.println(list);
+    }
+}
