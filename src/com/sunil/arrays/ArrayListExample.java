4c5f183aa7d10e9a27e304a21ffd0c4df0aebd5e
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-04 15:57:46 +0530

    sample example of arraylist  via printing couple of integers

---
+++ src/com/sunil/arrays/ArrayListExample.java
@@ -0,0 +1,19 @@
+package com.sunil.arrays;
+import java.util.ArrayList;
+
+public class ArrayListExample  {
+    public static void main(String[] args) {
+        //syntax for arraylist
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        System.out.println(list);
+
+    }
+
+}
