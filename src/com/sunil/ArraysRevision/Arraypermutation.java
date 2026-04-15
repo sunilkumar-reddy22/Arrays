c0dc9c2afbae8f5594bf1bf0f8da710e18470d6e
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-16 00:09:02 +0530

    array permutation just revisting for pattern recoginition

--- src/com/sunil/ArraysRevision/Arraypermutation.java
+++ src/com/sunil/ArraysRevision/Arraypermutation.java
@@ -1,16 +1,20 @@
 package com.sunil.ArraysRevision;
-
+import java.util.Arrays;
 public class Arraypermutation {
     public static void main(String[] args) {
         int[] nums = {0, 2, 1, 5, 3, 4};
-        meth(nums);
+        System.out.println(Arrays.toString(math(nums)));
+
     }
 
-    static int[] meth(int[] nums) {
+    static int[] math(int[] nums) {
+        int [] ans = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            int[] ans = new int[nums];
+          //  int[] ans = new int[nums];
             ans[i] = nums[nums[i]];
         }
+      return ans;
     }
+
 
 }
