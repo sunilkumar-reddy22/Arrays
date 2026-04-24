e7dbbaf4bf5f4a5dc7ff960a7af11e42e22bcbc6
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-24 15:15:09 +0530

    Added <= operator for bounds

--- src/com/sunil/leetcode/sumUptozero.java
+++ src/com/sunil/leetcode/sumUptozero.java
@@ -12,7 +12,7 @@ public class sumUptozero {
         if (n % 2 == 1) {
             nums[index++] = 0;
         }
-        for (int i = 1; i < n / 2; i++) {   //mistake i added 0 for the loop i should start from the 1 cause i am using 2 index++.
+        for (int i = 1; i <= n / 2; i++) {   //mistake i added 0 for the loop i should start from the 1 cause i am using 2 index++.
             nums[index++] = i;
             nums[index++] = -i;
         }
