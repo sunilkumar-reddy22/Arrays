17205db1c4cf30e420a55c267bcb61a96508f79e
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-04-24 08:09:07 +0530

    1480 Ruuningsum problem using prefixsum

---
+++ src/com/sunil/leetcode/RunningSum.java
@@ -0,0 +1,16 @@
+package com.sunil.leetcode;
+import java.util.Arrays;
+public class RunningSum {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] ans = sum(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] sum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
