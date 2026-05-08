f3c78318d1b00210c3fb9eb1c521a6ba68b8646c
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-08 11:25:25 +0530

    target element in matrix m X n,approched linear search,brute force method

---
+++ src/com/sunil/leetcode/FindtargetMatrix.java
@@ -0,0 +1,28 @@
+package com.sunil.leetcode;
+//brute force method
+public class FindtargetMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        int target = 5;
+       boolean result = Find(matrix, target);
+        System.out.println(result);
+    }
+
+    //return boolean.
+    static boolean Find(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
