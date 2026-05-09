34506c29b160ddd3334c2a42a04242a67a8bcb9d
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-09 22:15:12 +0530

    updated and operator

--- src/com/sunil/leetcode/FindtargetMatrix.java
+++ src/com/sunil/leetcode/FindtargetMatrix.java
@@ -16,7 +16,6 @@ public class FindtargetMatrix {
 
     //return boolean.
     static boolean Find(int[][] matrix, int target) {
-    if(int [] [] matrix = 0)
         for (int i = 0; i < matrix.length - 1; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == target) {
