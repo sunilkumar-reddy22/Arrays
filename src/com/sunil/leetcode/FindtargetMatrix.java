413ca38c6c0ec0819a1554cdf7894cba747a06b7
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-09 22:13:06 +0530

    Minor chnages

--- src/com/sunil/leetcode/FindtargetMatrix.java
+++ src/com/sunil/leetcode/FindtargetMatrix.java
@@ -9,13 +9,14 @@ public class FindtargetMatrix {
                 {10, 13, 14, 17, 24},
                 {18, 21, 23, 26, 30}
         };
-        int target = 5;
+        int target = 0;
        boolean result = Find(matrix, target);
         System.out.println(result);
     }
 
     //return boolean.
     static boolean Find(int[][] matrix, int target) {
+    if(int [] [] matrix = 0)
         for (int i = 0; i < matrix.length - 1; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == target) {
